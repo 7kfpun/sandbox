add71740cd4e0bd29bccb5bfde44f73e3e4aac9a
kf <7kfpun@gmail.com>
2025-01-11 23:27:06 +0800

    upt: add kody sdk

---
+++ kotlin-sandbox/build.gradle-kody.kts
@@ -0,0 +1,48 @@
+plugins {
+    kotlin("jvm") version "1.9.22"
+    kotlin("plugin.spring") version "1.9.22"
+    id("org.springframework.boot") version "3.2.2"
+    id("io.spring.dependency-management") version "1.1.4"
+}
+
+group = "com.kfpun"
+version = "0.0.1-SNAPSHOT"
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(21))
+    }
+}
+
+kotlin {
+    jvmToolchain(21)
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib")
+    developmentOnly("org.springframework.boot:spring-boot-devtools")
+
+    // Add your dependencies here
+    implementation("com.kodypay.grpc:kody-clientsdk-java:0.0.9")
+    implementation("io.grpc:grpc-netty-shaded:1.58.0")
+    implementation("io.grpc:grpc-protobuf:1.58.0")
+    implementation("io.grpc:grpc-stub:1.58.0")
+
+    implementation("org.apache.commons:commons-math3:3.6.1")
+}
+
+tasks.bootJar {
+    archiveFileName.set("app.jar")
+}
+
+tasks.register<Copy>("copyDependencies") {
+    from(configurations.runtimeClasspath)
+    into("build/dependencies")
+}
